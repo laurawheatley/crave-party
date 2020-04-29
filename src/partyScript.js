fd334983f798981b05e4403e1a85839f7a4f5584
laurawheatley <laura.eliza.wheatley@gmail.com>
2020-04-29 12:20:03 -0400

    Initial session ID func

--- src/partyScript.js
+++ src/partyScript.js
@@ -1,7 +1,8 @@
-
+var sessionID = ''
+var partyCreated = false
 /* setup page */
 
-var html =  '<div class="party-sidebar"><div class="sidebar-title">Crave Party</div><div class="sidebar-chat"></div><input type="text" class="text-input"></input></div>';
+var html =  '<div class="party-sidebar"><div class="sidebar-title">Crave Party<div class="sidebar-options">...</div></div><div class="sidebar-chat"></div><input type="text" class="text-input"></input></div>';
 $(".site").append(html);
 
 $('.sidebar-chat').append('<p>User1 has joined.</p>')
@@ -15,3 +16,28 @@ $('.text-input').on('keydown', function(event) {
         this.value = ""
     }
 })
+
+$('.sidebar-options').on('click', function(event) {
+    if (!partyCreated) {
+        var startNew = confirm('Start a new Crave Party?')
+        if (startNew) {
+            sessionID = pseudandomcode(8)
+            alert('The code for your party is: ' + sessionID)
+            partyCreated = true
+        } else {
+            sessionID = prompt("Enter the code of the Crave Party you wish to join:")
+        }
+    } else {
+        alert('The code for your party is: ' + sessionID)
+    }
+})
+
+function pseudandomcode(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
