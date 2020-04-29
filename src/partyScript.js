ef5fc025d9228de01bfd646ddbd109a248316de6
laurawheatley <laura.eliza.wheatley@gmail.com>
2020-04-29 11:19:06 -0400

    Added styling, initial textbook functionality for 1 user

--- src/partyScript.js
+++ src/partyScript.js
@@ -1,5 +1,17 @@
-console.log('BOINK')
 
-var html =  '<div>HELLO</div>';
-html = $.parseHTML(html);
-$(".site").append('<div>HELLO</div>');
+/* setup page */
+
+var html =  '<div class="party-sidebar"><div class="sidebar-title">Crave Party</div><div class="sidebar-chat"></div><input type="text" class="text-input"></input></div>';
+$(".site").append(html);
+
+$('.sidebar-chat').append('<p>User1 has joined.</p>')
+
+/* helper functions */
+
+$('.text-input').on('keydown', function(event) {
+    if (event.key == "Enter") {
+        console.log(event)
+        $('.sidebar-chat').append('<p>User1: ' + this.value + '</p>')
+        this.value = ""
+    }
+})
