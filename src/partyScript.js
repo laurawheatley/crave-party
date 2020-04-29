ac47830053750fae3407473e7ace587e221c9685
laurawheatley <laura.eliza.wheatley@gmail.com>
2020-04-29 12:39:21 -0400

    Finished setup before websockets

--- src/partyScript.js
+++ src/partyScript.js
@@ -1,16 +1,16 @@
 var sessionID = ''
 var partyCreated = false
+
 /* setup page */
 
 var html =  '<div class="party-sidebar"><div class="sidebar-title">Crave Party<div class="sidebar-options">...</div></div><div class="sidebar-chat"></div><input type="text" class="text-input"></input></div>';
 $(".site").append(html);
 $("input").prop('disabled', true);
 
-/* helper functions */
+/* event functions */
 
 $('.text-input').on('keydown', function(event) {
     if (event.key == "Enter") {
-        console.log(event)
         $('.sidebar-chat').append('<p>User1: ' + this.value + '</p>')
         this.value = ""
     }
@@ -19,29 +19,43 @@ $('.text-input').on('keydown', function(event) {
 $('.sidebar-options').on('click', function(event) {
 
     /* creation stage */
+
+    //TODO: this should eventually happen in a nice modal or something
+
     if (!partyCreated) {
         var startNew = confirm('Start a new Crave Party?')
         if (startNew) {
-            sessionID = pseudandomcode(8)
-            alert('The code for your party is: ' + sessionID)
-            partyCreated = true
-            userID = 1
-            $('.sidebar-chat').append('<p>User1 has joined.</p>')
-            $("input").prop('disabled', false);
-
+            createParty()
         } else {
             sessionID = prompt("Enter the code of the Crave Party you wish to join:")
-            //TODO: need to know how many users are here already to assign a userID
-            //userID = 1
-            //$('.sidebar-chat').append('<p>User1 has joined.</p>')
-            //$("input").prop('disabled', false);s
+            joinParty(sessionID)
         }
 
-    /* checking code */
+    /* code retrieval */
+
     } else {
         alert('The code for your party is: ' + sessionID)
     }
+    
 })
+
+/* helper functions */
+
+function createParty() {
+    sessionID = pseudandomcode(8)
+    alert('The code for your party is: ' + sessionID)
+    partyCreated = true
+    userID = 1
+    $('.sidebar-chat').append('<p>User1 has joined.</p>')
+    $("input").prop('disabled', false);
+}
+
+function joinParty() {
+    //TODO: need to know how many users are here already to assign a userID
+    //userID = 1
+    //$('.sidebar-chat').append('<p>User1 has joined.</p>')
+    //$("input").prop('disabled', false);s
+}
 
 function pseudandomcode(length) {
     var result = '';
